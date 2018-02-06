f15e0db411e8f99cbfb7a72e1d33afbacb168d02
Sairam396 <sairamchowdary01@gmail.com>
2018-02-05 22:17:11 -0800

    Initial commit

---
+++ app/src/main/java/sairamchowdary/com/flickrapp/FlickrImageViewHolder.java
@@ -0,0 +1,22 @@
+package sairamchowdary.com.flickrapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Sairam Chowdary on 1/7/2018.
+ */
+
+public class FlickrImageViewHolder extends RecyclerView.ViewHolder {
+
+    protected ImageView thumbnail;
+    protected TextView title;
+
+    public FlickrImageViewHolder(View view) {
+        super(view);
+        this.thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        this.title = (TextView) view.findViewById(R.id.title);
+    }
+}
---
+++ app/src/main/java/sairamchowdary/com/flickrapp/MainActivityFragment.java
@@ -0,0 +1,10 @@
+package sairamchowdary.com.flickrapp;
+
+import android.app.Activity;
+
+/**
+ * Created by Sairam Chowdary on 1/8/2018.
+ */
+
+public class MainActivityFragment extends Activity {
+}
